8d8fb912491555e04f113a7bb8dc3dc7032eae9c
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2018-11-01 11:18:36 +0900

    add file log in test-host.

--- test-host/api/controllers/order-csv.js
+++ test-host/api/controllers/order-csv.js
@@ -7,5 +7,9 @@ module.exports = {
 function importCsv(req, res) {
   var message = 'received!';
 
+  // output formData
+  console.log('file:', req.swagger.params.file);
+  console.log('content:', req.swagger.params.file.value.buffer.toString());
+
   res.json(message);
 }
